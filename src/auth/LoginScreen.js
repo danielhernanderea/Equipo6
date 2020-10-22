be5ba3d35901cb5e72c4e62ef294a8afeb8435dd
ThRangel <the.beatles.lma2@gmail.com>
2020-10-22 14:55:08 -0500

    android key

--- src/auth/LoginScreen.js
+++ src/auth/LoginScreen.js
@@ -28,9 +28,8 @@ export class  LoginScreen extends React.Component {
    
     this.state= {
       email: "",
-      password: "",
-      isModalVisible: false,
-      textModal: "" 
+      password: ""
+    
      
     };
   
@@ -38,110 +37,13 @@ export class  LoginScreen extends React.Component {
 
  
 
-  componentDidMount(){
-   
- 
-  
-
-    const unsubscribe = NetInfo.addEventListener(state => {
-      console.log("Is connected?", state.isConnected);
-      if(state.isConnected){
-        console.log("SÍ ESTÁ CONECTADO A INTERENET")
-      }else{
-        console.log("NO ESTÁ CONECTADO A INTERENET")
-        Alert.alert("No está conectado a internet, se necesita conexión para que la app funcione")
-      }
-    });
-
-    return () =>{
-      unsubscribe();
-    }
-    
-    
-  }
-
-
-  
-  
- setIDstorage = async (key, value) => {
-    try {
-      await AsyncStorage.setItem(
-       key,
-       JSON.stringify(value)
-      );
-      console.log(" guardando el id y el usertype");
-    } catch (error) {
-      console.log("Hubo un error al guadar la data"+ error);
-    }
-    
-  };
-
-  saveStorage = ( ID, USERTYPE) =>{
-    this.setIDstorage("Login",{ id : ID, userType: USERTYPE})
-  }
-
-  
-
-
   ChangeModalVisibility = b=>  this.setState({isModalVisible:  b});
   ChangeTextModal = text => this.setState({textModal: text});
   onChangeEmail = email => this.setState({email});
   onChangePassword = password => this.setState({password});
 
 
-/*
-onSubmit = async () =>{
-    
-    const {email, password} = this.state;
 
-    console.log(email)
-    console.log(password)
-
-  if(email.length>0 && password.length>0)
-  {
-    try{
-        var resultAPI = await axios.get(''
-         ).then(response =>{
-               
-                
-                  let result2 = response.data;
-                  console.log(result2)
-                
-                  
-                  if(result2.success){
-                   
-                      
-                        
-                        console.log(result2);
-                        let name = result2.name
-                        let idUsuario= result2.id
-                        let userType = result2.userType
-                        this.saveStorage(idUsuario, userType)
-                        console.log("Guardando el id y el usertype: "+idUsuario+"usertype : "+userType)
-                    
-                       // this.props.navigation.navigate('HomeApp') 
-                        Alert.alert(' BIENVENIDO '+ name)
-                      
-                       
-                  }else{
-                         Alert.alert(' Informacion de acceso incorrecta ')
-                        console.log("info incorrecta de aceso")
-                       
-                   }
-        });
-      
-
-    }catch(error){
-      console.log("hay un error: "+error)
-      Alert.alert("Hubo un error. Intentelo más tarde.")
-     
-    }
-  }else{
-    console.log("no pudo entrar a mifuncion de la peticion");
-    Alert.alert("POR FAVOR LLENA TODOS LOS CAMPOS")
-  }
-};
-*/
 
 onSubmit = async () =>{
     
@@ -207,7 +109,7 @@ if(email.length>0 && password.length>0)
        return (
        
        <SafeAreaView style={{flex:1}} >
-       <ImageBackground source={IMAGE.FONDO_HOME_FIDE} style= {styles.backgroundContainer}>
+       <ImageBackground source={IMAGE.MI_FONDO} style= {styles.backgroundContainer}>
        <View style={{  alignItems:"center",width:wp('100%'), height: hp('100%'), justifyContent: "center"
                         
                        }}>
@@ -250,7 +152,7 @@ if(email.length>0 && password.length>0)
              <TouchableOpacity style = {styles.botonLogin}
                                 //  onPress={this.onSubmit}
                                 onPress={() => 
-                                this.onSubmit}
+                                this.onSubmit()}
               >
                    <Text style={styles.text}>ENTRAR</Text>
               </TouchableOpacity>
