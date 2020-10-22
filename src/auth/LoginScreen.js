5a4d4b46d9ac752978961e81e9263ba1e84b1dff
ThRangel <the.beatles.lma2@gmail.com>
2020-10-22 12:24:43 -0500

    Consumo del API login

--- src/auth/LoginScreen.js
+++ src/auth/LoginScreen.js
@@ -89,7 +89,7 @@ export class  LoginScreen extends React.Component {
   onChangePassword = password => this.setState({password});
 
 
-
+/*
 onSubmit = async () =>{
     
     const {email, password} = this.state;
@@ -141,8 +141,58 @@ onSubmit = async () =>{
     Alert.alert("POR FAVOR LLENA TODOS LOS CAMPOS")
   }
 };
+*/
+
+onSubmit = async () =>{
+    
+  const {email, password} = this.state;
+
+  console.log(email)
+  console.log(password)
+
+if(email.length>0 && password.length>0)
+{
+  try{
 
 
+ await   fetch('http://localhost:3000/Login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    }).then((response) => response.json()).then((json) => {
+     console.log(json)
+      if (json.status == 200){
+        console.log(json)
+        this.props.navigation.navigate('HomeApp') 
+        Alert.alert(json.message)
+      }else{
+        Alert.alert(' Informacion de acceso incorrecta ')
+       console.log("info incorrecta de aceso")
+      
+  }
+    })
+    //++++++++++++++++
+
+
+     
+    
+
+  }catch(error){
+    console.log("hay un error: "+error)
+    Alert.alert("Hubo un error. Intentelo más tarde.")
+   
+  }
+}else{
+  console.log("no pudo entrar a mifuncion de la peticion");
+  Alert.alert("POR FAVOR LLENA TODOS LOS CAMPOS")
+}
+};
 
 
 
@@ -157,7 +207,7 @@ onSubmit = async () =>{
        return (
        
        <SafeAreaView style={{flex:1}} >
-       <ImageBackground source={IMAGE.MI_FONDO} style= {styles.backgroundContainer}>
+       <ImageBackground source={IMAGE.FONDO_HOME_FIDE} style= {styles.backgroundContainer}>
        <View style={{  alignItems:"center",width:wp('100%'), height: hp('100%'), justifyContent: "center"
                         
                        }}>
@@ -170,7 +220,7 @@ onSubmit = async () =>{
                    <Image source ={IMAGE.ICON_LOGIN_USUARIO} style = {styles.inputIcon}/>
                    <TextInput
                      style={styles.input}
-                     placeholder={'CORREO ELECTRÓNICO'}
+                     placeholder={'CORREO ELECTRÓNICO jaja'}
                      placeholderTextColor={'#6f6f6e'}
                      underlineColorAndroid = 'transparent'
                      onChangeText={this.onChangeEmail} 
@@ -200,7 +250,7 @@ onSubmit = async () =>{
              <TouchableOpacity style = {styles.botonLogin}
                                 //  onPress={this.onSubmit}
                                 onPress={() => 
-                                  this.props.navigation.navigate('HomeApp') }
+                                this.onSubmit}
               >
                    <Text style={styles.text}>ENTRAR</Text>
               </TouchableOpacity>
